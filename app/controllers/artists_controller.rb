0b0b3e0ac3c5dbe068136b8148b69d17007cf722
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-07-27 04:26:59 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,6 +8,7 @@ class ArtistsController < ApplicationController
       end
 
       def create
+        binding.pry
         @artist = Artist.create(artist_params(:name, :bio))
         redirect_to artist_path(@artist)
       end
